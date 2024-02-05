d6d55d104e4affabd847bb66cf2033069ecc90d5
ashik pa <ashikpa003@gmail.com>
2024-02-05 18:29:09 +0400

    adding the card details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 import NavigationBar from './components/NavigationBar';
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <NavigationBar/>
       <HomePage/>
+      <Footer/>
     </div>
   );
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <Row className="mt-5 ms-5 mb-5 me-5">
+        <Col sm={12} md={6} lg={4} xl={3}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
