bf76c0e78dfcda7b225ed62c010fe2dfd8ddab06
ashik pa <ashikpa003@gmail.com>
2024-02-06 14:21:16 +0400

    implement  loading functionality

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
 
-function HomePage({data}) {
-  // console.log(data);
+function HomePage({ data }) {
+  const [loading, SetLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      SetLoading(false);
+    }, 1000);
+  }, []);
   return (
     <>
-      <Row className="mt-5 ms-5 mb-5 me-5 justify-content-center">
-        { data?.map((item)=>{
-          const {id,image,title,price,description} = item
-          return <Col className="mb-4" key={id} sm={12} md={6} lg={4} xl={3}>
-          <Card  style={{ width: "19rem",height:'40rem' }}>
-            <Card.Img variant="top" src={image} height={'300px'} />
-            <Card.Body>
-              <Card.Title>{title.slice(0,100)}...</Card.Title><p className="fw-bolder">${price}</p>
-              <Card.Text>
-                <p>{description.slice(0,150)}...</p>
-              </Card.Text>
-              <Button variant="primary">Buy Now</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        })
-          
-        }
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Row className="mt-5 ms-5 mb-5 me-5 justify-content-center">
+          {data?.map((item) => {
+            const { id, image, title, price, description } = item;
+            return (
+              <Col className="mb-4" key={id} sm={12} md={6} lg={4} xl={3}>
+                <Card style={{ width: "19rem", height: "40rem" }}>
+                  <Card.Img variant="top" src={image} height={"300px"} />
+                  <Card.Body>
+                    <Card.Title>{title.slice(0, 100)}...</Card.Title>
+                    <p className="fw-bolder">${price}</p>
+                    <Card.Text>
+                      <p>{description.slice(0, 150)}...</p>
+                    </Card.Text>
+                    <Button variant="primary">Buy Now</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 }
